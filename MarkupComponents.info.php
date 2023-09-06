6a5edbac57be7695678542e32310abf4f9e14ac9
romaincazier <romain@eprc.studio>
2023-09-06 17:48:38 +0200

    Added module’s informations

---
+++ MarkupComponents.info.php
@@ -0,0 +1,12 @@
+<?php namespace ProcessWire;
+
+$info = [
+	"title" => "Components",
+	"version" => "0.0.1",
+	"summary" => "Set of functions allowing you to segment your code into components/snippets.",
+	"author" => "EPRC",
+	"href" => "https://github.com/eprcstudio/MarkupComponents",
+	"singular" => true,
+	"autoload" => "template!=admin",
+	"icon" => "puzzle-piece",
+];
